f2cca33acfb0daf80b4a2dd2941e843a20df19d7
wbarcellosn <wbarcellosn@gmail.com>
2023-11-03 21:42:41 -0300

    Atividade realizada

--- main.js
+++ main.js
@@ -1,42 +1,126 @@
-function Individuo (nome, idade, funcao) {
-    this.nome = nome;
-    this.idade = idade;
-    this.funcao = funcao;
-}
+const prompt = require('prompt-sync')();
 
-function Aluno (nome, idade, funcao, turma) {
-    this.turma = turma;
-
-    this.apresentaAluno = () =>{
-        console.log(`Olá! Eu sou ${nome}, tenho ${idade} e sou ${funcao} da ${turma}.`)
+class Aluno {
+    constructor(nomeDoAluno, notaDoAluno) {
+        this.nome = nomeDoAluno;
+        this.nota = notaDoAluno;
     }
 
-    Individuo.call(this, nome, idade, funcao)
 }
 
-function Professor (nome, idade, funcao, disciplina, rede, salario) {
-    this.disciplina = disciplina;
-    
-    this.apresentaProfessor = () =>{
-        console.log(`Olá! Eu sou ${nome}, tenho ${idade} e sou ${funcao} de ${disciplina}.`)
+const alunos = [];
+
+function main() {
+    console.clear();
+    let opcao;
+
+    console.log(`Sistema de cadastramento de notas:
+        
+        1 - Cadastrar nova nota;
+        2 - Visualizar alunos cadastrados;
+        3 - Visualizar alunos aprovados;
+        4 - Visualizar alunos reprovados;
+        0 - Sair \n`);
+
+    opcao = prompt('Digite a opção desejada: ');
+
+    if (opcao == '1') {
+        console.clear();
+        cadastrarAluno();
+        console.log('\nAluno e nota cadastrados com sucesso! \n');
+        prompt('Pressione ENTER para voltar ao menu principal.');
+        console.clear();
+        main();
     }
 
-    Individuo.call(this, nome, idade, funcao)
+    else if (opcao == '2') {
+        console.clear();
+        console.table(alunos);
+        console.log('\n');
+        prompt('Pressione ENTER para voltar ao menu principal.');
+        console.clear();
+        main();
+    }
+
+    else if (opcao == '3') {
+        console.clear();
+        retornaAprovados();
+        console.log('\n');
+        prompt('Pressione ENTER para voltar ao menu principal.');
+        console.clear();
+        main();
+    }
+
+    else if (opcao == '4') {
+        console.clear();
+        retornaReprovados();
+        console.log('\n');
+        prompt('Pressione ENTER para voltar ao menu principal.');
+        console.clear();
+        main();
+    }
+
+    else if (opcao == '0') {
+        console.clear();
+    }
+
+    else {
+        console.clear();
+        console.log('Opção inválida!\n');
+        prompt('Pressione ENTER para voltar ao menu principal.');
+        main();
+    }
+} 
+
+main();
+
+
+function cadastrarAluno() {
+    let nome = prompt('Digite o nome do aluno: ');
+    let nota = prompt('Digite a nota do aluno: ');
+
+    const alunoCadastrado = new Aluno(nome, nota);
+
+    if (nota >= 6 && nota <= 10) {
+        alunoCadastrado.situacao = 'Aprovado(a)'
+        alunos.push(alunoCadastrado)
+    }
+
+    else if (nota >= 0 && nota < 6) {
+        alunoCadastrado.situacao = 'Reprovado(a)'
+        alunos.push(alunoCadastrado)
+    }
+
+    else {
+        console.log('\nApenas notas de 0 a 10 são aceitas, por favor, digite uma nota válida.\n');
+        prompt('Pressione ENTER para reiniciar.');
+        console.clear();
+        cadastrarAluno();
+    }
+
+
+}
+
+function retornaAprovados() {
+    const alunosAprovados = alunos.filter((aluno) => {
+        return aluno.situacao == "Aprovado(a)"
+    })
+
+    console.clear()
+    console.log(`Estão aprovados um total de ${alunosAprovados.length} alunos.`)
+    console.log("São eles:");
+    console.table(alunosAprovados);
+}
+
+function retornaReprovados() {
+    const alunosReprovados = alunos.filter((aluno) => {
+        return aluno.situacao == "Reprovado(a)"
+    })
+
+    console.clear()
+    console.log(`Estão reprovados um total de ${alunosReprovados.length} alunos.`)
+    console.log("São eles:");
+    console.table(alunosReprovados);
 }
 
 
-const aluno1 = new Aluno('Pedro', '10 anos', 'aluno(a)', '6ª B');
-const aluno2 = new Aluno('Maria', '9 anos', 'aluno(a)', '5ª A');
-const aluno3 = new Aluno('João', '12 anos', 'aluno(a)', '7ª B');
-
-const professor1 = new Professor('Sérgio', '35 anos', 'professor(a)', 'Matemática');
-const professor2 = new Professor('Thaila', '39 anos', 'professor(a)', 'Português');
-const professor3 = new Professor('Mariana', '25 anos', 'professor(a)', 'Física');
-
-aluno1.apresentaAluno();
-aluno2.apresentaAluno();
-aluno3.apresentaAluno();
-console.log('\n-----------------------\n');
-professor1.apresentaProfessor();
-professor2.apresentaProfessor();
-professor3.apresentaProfessor();
